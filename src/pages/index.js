466eecff61c06b1ab81857ee97f9ad6de1dc15c6
Junwoo <jwoh@uwaterloo.ca>
2024-03-01 16:42:43 -0500

    grammar fix2

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
         <div className = {styles.aboutText}>
         <h1>ABOUT US</h1>
         <h3>Born from the passion for creative storytelling and captivating visuals, we aim to elevate your story through our vision. </h3>
-        <h3>What started as a hobby for a single artist now transformed into a team working together to create memorable lasting visuals. A group of engineering students who also found passion in storytelling came together to form JWD Creative House. The constant innovating mindset of an engineer is what sets us apart. Your story should be told in it's unique way, and we'll find a way to craft it with precision.</h3>
-        <h3>So whether you are a fellow artist, small business, or just someone who's story needs to be told to the world, we'd love for you to let us craft it for you.</h3>
+        <h3>What started as a hobby for a single artist now transformed into a team working together to create memorable lasting visuals. A group of engineering students who also found passion in storytelling came together to form JWD Creative House. The constant innovating mindset of an engineer is what sets us apart. Your story should be told in it&#39;s unique way, and we&#39;ll find a way to craft it with precision.</h3>
+        <h3>So whether you are a fellow artist, small business, or just someone whose story needs to be told to the world, we&#39;d love for you to let us craft it for you.</h3>
         </div>
       </div>
       <div className = {styles.work}>
