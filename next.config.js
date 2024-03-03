863446910b69bed44d7a475887bbc2619baccf63
Junwoo <jwoh@uwaterloo.ca>
2024-03-02 20:53:01 -0500

    some fixes

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const { withNextVideo } = require('next-video/process');
- 
-const nextConfig = {}; // Your current Next Config object
- 
-module.exports = withNextVideo(nextConfig);
