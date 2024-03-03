5d0be16e6ab8be1defdc4598bfc5a5e892af8bae
Junwoo <jwoh@uwaterloo.ca>
2024-03-02 21:11:52 -0500

    seo revision

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,12 +6,6 @@ import Head from 'next/head'
 function Contact() {
   return (
     <div className = {styles.contactWrapper} id = "contact">
-      <Head>
-        <title>CONTACT - JWD CREATIVE HOUSE</title>
-        <meta name="description" content="CONTACT US TO GET STARTED ON YOUR STORY" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className = {styles.contactRight}>
         <h1>CONTACT US</h1>
         <h3>We&#39;d love to capture your story.</h3>
